8ac0469329014442d83ec2e83e6df65fda7142f4
Sigbenu <oghenero.sigbenu@seerglobalsolutions.com>
2026-01-12 06:42:58 +0100

    updates

--- src/Components/CreateBudgetModal.jsx
+++ src/Components/CreateBudgetModal.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { FaTimes, FaCalculator } from "react-icons/fa";
 export default function CreateBudgetModal() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   const features = [
     {
